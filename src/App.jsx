ab29a2044cc0e9ea776f16860187b990e3cad693
Serhii Shabala <sergei181989@gmail.com>
2023-06-22 16:33:02 +0300

    :bug: fix: commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Home } from './pages';
 
 export const App = () => {
   return (
+    // TheNavigation component
     <Routes>
       <Route
         path='/'
         element={<Home />}
       />
     </Routes>
+    // TheFooter component
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 export const Home = () => {
   return (
     <>
-      {/* The Navigation component */}
       <h1>Home</h1>
-      {/* The Footer component */}
     </>
   );
 };
