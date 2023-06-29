c9617e79c72541fbe9ef1c0209943efa7a392291
ThunderFlight <igoreshkin2006@gmail.com>
2023-06-29 11:37:53 +0300

    add: animation adaptive to  homeUsefulArticles

--- src/components/Home/HomeUsefulAricles/HomeUsefulArticles.jsx
+++ src/components/Home/HomeUsefulAricles/HomeUsefulArticles.jsx
@@ -1,35 +1,81 @@
+import exterior from '../../../images/homeUsefulArticlesExterior.jpg';
+import interior from '../../../images/homeUsefulArticlesInterior.jpg';
+import interiorSecond from '../../../images/homeUsefulArticlesInteriorSecond.jpg';
 import { CustomArrow } from '../../common';
 import styles from './HomeUsefulArticles.module.scss';
 
 export const HomeUsefulArticles = () => {
   return (
     <section className={styles.usefulArticles}>
-      <div className={styles.usefulArticles__wrapperTitle}>
+      <div className={styles.usefulArticles__titleUseful}>
         <h2>Useful articles</h2>
-        <a>
-          View all articles <CustomArrow className={styles.customArrowGray} />
-        </a>
+        <span>
+          View all articles
+          <CustomArrow className={styles.usefulArticles__titleUseful_grayArrow} />
+        </span>
       </div>
-      <div className={styles.usefulArticles__wrapperImage}>
-        <div className={styles.usefulArticles__wrapperImage_exterior}>
-          <h2>Discover Architecture</h2>
+      <div className={styles.usefulArticles__divImagesBlock}>
+        <div className={styles.usefulArticles__allImgWrapper}>
+          <img
+            src={interiorSecond}
+            alt='interiorSecond'
+            className={styles.usefulArticles__allImgWrapper_img}
+          />
+
+          <div className={styles.usefulArticles__BorderMenuWrap}>
+            <p className={styles.usefulArticles__BorderMenuWrap_date}>Jule 03, 2022</p>
+            <h2 className={styles.usefulArticles__BorderMenuWrap_title}>Discover Architecture</h2>
+            <p className={styles.usefulArticles__BorderMenuWrap_about}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such
+            </p>
+            <label className={styles.usefulArticles__BorderMenuWrap_link}>
+              Learn more
+              <CustomArrow className={styles.usefulArticles__BorderMenuWrap_yellowArrow} />
+            </label>
+          </div>
         </div>
 
-        <div className={styles.usefulArticles__wrapperImage_border}>
-          <p className={styles.usefulArticles__wrapperImage_border_date}>jule 03,2022</p>
-          <h3 className={styles.usefulArticles__wrapperImage_border_title}>
-            Discover Architecture
-          </h3>
-          <p className={styles.usefulArticles__wrapperImage_border_about}>
-            Projects for many large domestic and foreign corporations, enterprises in many elds such
-          </p>
-          <a className={styles.usefulArticles__wrapperImage_border_link}>
-            Learn more <CustomArrow className={styles.customArrowYellow} />
-          </a>
+        <div className={styles.usefulArticles__allImgWrapper}>
+          <img
+            src={exterior}
+            alt='exterior'
+            className={styles.usefulArticles__allImgWrapper_img}
+          />
+
+          <div className={styles.usefulArticles__BorderMenuWrap}>
+            <p className={styles.usefulArticles__BorderMenuWrap_date}>Jule 03, 2022</p>
+            <h2 className={styles.usefulArticles__BorderMenuWrap_title}>Discover Architecture</h2>
+            <p className={styles.usefulArticles__BorderMenuWrap_about}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such
+            </p>
+            <label className={styles.usefulArticles__BorderMenuWrap_link}>
+              Learn more
+              <CustomArrow className={styles.usefulArticles__BorderMenuWrap_yellowArrow} />
+            </label>
+          </div>
         </div>
 
-        <div className={styles.usefulArticles__wrapperImage_interior}>
-          <h2>Discover Architecture</h2>
+        <div className={styles.usefulArticles__allImgWrapper}>
+          <img
+            src={interior}
+            alt='interior'
+            className={styles.usefulArticles__allImgWrapper_img}
+          />
+
+          <div className={styles.usefulArticles__BorderMenuWrap}>
+            <p className={styles.usefulArticles__BorderMenuWrap_date}>Jule 03, 2022</p>
+            <h2 className={styles.usefulArticles__BorderMenuWrap_title}>Discover Architecture</h2>
+            <p className={styles.usefulArticles__BorderMenuWrap_about}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such
+            </p>
+            <label className={styles.usefulArticles__BorderMenuWrap_link}>
+              Learn more
+              <CustomArrow className={styles.usefulArticles__BorderMenuWrap_yellowArrow} />
+            </label>
+          </div>
         </div>
       </div>
     </section>
