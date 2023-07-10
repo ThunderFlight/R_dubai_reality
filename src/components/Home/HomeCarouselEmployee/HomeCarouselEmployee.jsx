11c363e6cceba97c7761559e4845bac27a6c6059
ThunderFlight <igoreshkin2006@gmail.com>
2023-07-10 20:33:38 +0300

    fix: adaptive change swiperSlide

--- src/components/Home/HomeCarouselEmployee/HomeCarouselEmployee.jsx
+++ src/components/Home/HomeCarouselEmployee/HomeCarouselEmployee.jsx
@@ -4,7 +4,6 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import paul from '../../../images/HomeCarouselEmployeePaul.jpg';
-import quotes from '../../../images/HomeCarouselEmployeeQuotes.svg';
 import robert from '../../../images/HomeCarouselEmployeeRobert.jpg';
 import { Quotes } from '../../common/Quotes/Quotes';
 import styles from './HomeCarouselEmployee.module.scss';
@@ -18,101 +17,111 @@ export const HomeCarouselEmployee = () => {
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
-        className='mySwiper'>
-        <SwiperSlide>
-          <div className={styles.carouselEmployee__employeeCard}>
-            <div className={styles.carouselEmployee__wrapperAvatar}>
-              <img
-                src={robert}
-                alt='robert'
-                className={styles.carouselEmployee__wrapperAvatar_avatar}
-              />
-              <Quotes className={styles.carouselEmployee__wrapperAvatar_quotes} />
-            </div>
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
 
-            <div className={styles.carouselEmployee__employeeDesk}>
-              <h2 className={styles.carouselEmployee__employeeDesk_name}>Paul</h2>
-              <p className={styles.carouselEmployee__employeeDesk_owner}>
-                Owner in <a>Paul.com</a>
-              </p>
-              <p className={styles.carouselEmployee__employeeDesk_about}>
-                Projects for many large domestic and foreign corporations, enterprises in many elds
-                such as nance, banking, F&B, education, communication.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.carouselEmployee__employeeCard}>
-            <img
-              src={paul}
-              alt='paul'
-              className={styles.carouselEmployee__employeeCard_avatar}
-            />
-            <img
-              src={quotes}
-              alt='Quotes'
-              className={styles.carouselEmployee__employeeCard_quotes}
-            />
-            <div className={styles.carouselEmployee__employeeDesk}>
-              <h2 className={styles.carouselEmployee__employeeDesk_name}>Paul</h2>
-              <p className={styles.carouselEmployee__employeeDesk_owner}>
-                Owner in <a>Paul.com</a>
-              </p>
-              <p className={styles.carouselEmployee__employeeDesk_about}>
-                Projects for many large domestic and foreign corporations, enterprises in many elds
-                such as nance, banking, F&B, education, communication.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.carouselEmployee__employeeCard}>
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+
+          2560: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Pagination]}
+        className={styles.mySwiper}
+        style={{ paddingTop: '60px' }}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
+          <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={robert}
               alt='robert'
-              className={styles.carouselEmployee__employeeCard_avatar}
+              className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <img
-              src={quotes}
-              alt='Quotes'
-              className={styles.carouselEmployee__employeeCard_quotes}
-            />
-            <div className={styles.carouselEmployee__employeeDesk}>
-              <h2 className={styles.carouselEmployee__employeeDesk_name}>Paul</h2>
-              <p className={styles.carouselEmployee__employeeDesk_owner}>
-                Owner in <a>Paul.com</a>
-              </p>
-              <p className={styles.carouselEmployee__employeeDesk_about}>
-                Projects for many large domestic and foreign corporations, enterprises in many elds
-                such as nance, banking, F&B, education, communication.
-              </p>
-            </div>
+            <Quotes className={styles.carouselEmployee__avatarWrapper_quotes} />
+          </div>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Robert</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Apple Inc.</a>
+            </p>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such as nance, banking, F&B, education, communication.
+            </p>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.carouselEmployee__employeeCard}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
+          <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={paul}
               alt='paul'
-              className={styles.carouselEmployee__employeeCard_avatar}
+              className={styles.carouselEmployee__avatarWrapper_avatar}
             />
+            <Quotes className={styles.carouselEmployee__avatarWrapper_quotes} />
+          </div>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Paul</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Paul.com</a>
+            </p>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such as nance, banking, F&B, education, communication.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
+          <div className={styles.carouselEmployee__avatarWrapper}>
             <img
-              src={quotes}
-              alt='Quotes'
-              className={styles.carouselEmployee__employeeCard_quotes}
+              src={robert}
+              alt='robert'
+              className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <div className={styles.carouselEmployee__employeeDesk}>
-              <h2 className={styles.carouselEmployee__employeeDesk_name}>Paul</h2>
-              <p className={styles.carouselEmployee__employeeDesk_owner}>
-                Owner in <a>Paul.com</a>
-              </p>
-              <p className={styles.carouselEmployee__employeeDesk_about}>
-                Projects for many large domestic and foreign corporations, enterprises in many elds
-                such as nance, banking, F&B, education, communication.
-              </p>
-            </div>
+            <Quotes className={styles.carouselEmployee__avatarWrapper_quotes} />
+          </div>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Robert</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Apple Inc.</a>
+            </p>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such as nance, banking, F&B, education, communication.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
+          <div className={styles.carouselEmployee__avatarWrapper}>
+            <img
+              src={paul}
+              alt='paul'
+              className={styles.carouselEmployee__avatarWrapper_avatar}
+            />
+            <Quotes className={styles.carouselEmployee__avatarWrapper_quotes} />
+          </div>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Paul</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Paul.com</a>
+            </p>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
+              Projects for many large domestic and foreign corporations, enterprises in many elds
+              such as nance, banking, F&B, education, communication.
+            </p>
           </div>
         </SwiperSlide>
       </Swiper>
