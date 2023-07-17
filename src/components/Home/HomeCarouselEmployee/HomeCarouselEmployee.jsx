7950c2366d827309c8ec237fc30b9626a7c9bb8c
ThunderFlight <igoreshkin2006@gmail.com>
2023-07-17 13:25:16 +0300

    :hammer: change import grid and pagination in carouselEmployee and Lastes

--- src/components/Home/HomeCarouselEmployee/HomeCarouselEmployee.jsx
+++ src/components/Home/HomeCarouselEmployee/HomeCarouselEmployee.jsx
@@ -5,13 +5,20 @@ import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import paul from '../../../images/HomeCarouselEmployeePaul.jpg';
 import robert from '../../../images/HomeCarouselEmployeeRobert.jpg';
-import { Quotes } from '../../common/Quotes/Quotes';
 import styles from './HomeCarouselEmployee.module.scss';
 
 export const HomeCarouselEmployee = () => {
   return (
     <section className={styles.carouselEmployee}>
       <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className={styles.mySwiper}
+        style={{ paddingTop: '60px' }}
         breakpoints={{
           320: {
             slidesPerView: 1,
@@ -19,95 +26,98 @@ export const HomeCarouselEmployee = () => {
           },
           768: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            spaceBetween: 30,
           },
+
           1024: {
             slidesPerView: 3,
+            spaceBetween: 39,
           },
-        }}
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className={styles.carouselEmployee__swiper}
-        style={{ paddingTop: '60px', overflow: 'hidden' }}>
-        <SwiperSlide className={styles.carouselEmployee__swiperElement}>
+
+          2560: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+        }}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
           <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={robert}
               alt='robert'
               className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <Quotes className={styles.carouselEmployee__avatarWrapper_quote} />
+            <span className={styles.carouselEmployee__avatarWrapper_quotes}>”</span>
           </div>
-          <div className={styles.carouselEmployee__descOfEmployee}>
-            <h1 className={styles.carouselEmployee__descOfEmployee_name}>Robert</h1>
-            <p className={styles.carouselEmployee__descOfEmployee_ownerOf}>
-              Owner in <a href='#'>Apple Inc</a>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Robert</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Apple Inc.</a>
             </p>
-            <p className={styles.carouselEmployee__descOfEmployee_about}>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such as nance, banking, F&B, education, communication.
             </p>
           </div>
         </SwiperSlide>
-        <SwiperSlide className={styles.carouselEmployee__swiperElement}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
           <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={paul}
               alt='paul'
               className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <Quotes className={styles.carouselEmployee__avatarWrapper_quote} />
+            <span className={styles.carouselEmployee__avatarWrapper_quotes}>”</span>
           </div>
-          <div className={styles.carouselEmployee__descOfEmployee}>
-            <h1 className={styles.carouselEmployee__descOfEmployee_name}>Paul</h1>
-            <p className={styles.carouselEmployee__descOfEmployee_ownerOf}>
-              Owner in <a href='#'>Paul.com</a>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Paul</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Paul.com</a>
             </p>
-            <p className={styles.carouselEmployee__descOfEmployee_about}>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such as nance, banking, F&B, education, communication.
             </p>
           </div>
         </SwiperSlide>
-        <SwiperSlide className={styles.carouselEmployee__swiperElement}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
           <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={robert}
               alt='robert'
               className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <Quotes className={styles.carouselEmployee__avatarWrapper_quote} />
+            <span className={styles.carouselEmployee__avatarWrapper_quotes}>”</span>
           </div>
-          <div className={styles.carouselEmployee__descOfEmployee}>
-            <h1 className={styles.carouselEmployee__descOfEmployee_name}>Robert</h1>
-            <p className={styles.carouselEmployee__descOfEmployee_ownerOf}>
-              Owner in <a href='#'>Apple Inc</a>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Robert</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Apple Inc.</a>
             </p>
-            <p className={styles.carouselEmployee__descOfEmployee_about}>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such as nance, banking, F&B, education, communication.
             </p>
           </div>
         </SwiperSlide>
-        <SwiperSlide className={styles.carouselEmployee__swiperElement}>
+        <SwiperSlide className={styles.carouselEmployee__slideWrapper}>
           <div className={styles.carouselEmployee__avatarWrapper}>
             <img
               src={paul}
               alt='paul'
               className={styles.carouselEmployee__avatarWrapper_avatar}
             />
-            <Quotes className={styles.carouselEmployee__avatarWrapper_quote} />
+            <span className={styles.carouselEmployee__avatarWrapper_quotes}>”</span>
           </div>
-          <div className={styles.carouselEmployee__descOfEmployee}>
-            <h1 className={styles.carouselEmployee__descOfEmployee_name}>Paul</h1>
-            <p className={styles.carouselEmployee__descOfEmployee_ownerOf}>
-              Owner in <a href='#'>Paul.com</a>
+
+          <div className={styles.carouselEmployee__aboutWrapper}>
+            <h2 className={styles.carouselEmployee__aboutWrapper_name}>Paul</h2>
+            <p className={styles.carouselEmployee__aboutWrapper_ownerOf}>
+              Owner in <a>Paul.com</a>
             </p>
-            <p className={styles.carouselEmployee__descOfEmployee_about}>
+            <p className={styles.carouselEmployee__aboutWrapper_autobiography}>
               Projects for many large domestic and foreign corporations, enterprises in many elds
               such as nance, banking, F&B, education, communication.
             </p>
